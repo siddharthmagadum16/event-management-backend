c1dcb2fa39c6ded955b2edba07d5d49bf04a5dcc
Siddharth Magadum <siddharthmagadum16@gmail.com>
2021-11-20 14:23:10 +0530

    database : from env.database

--- db.js
+++ db.js
@@ -6,7 +6,7 @@ const pool = new Pool ({
     user : process.env.USER,
     password : process.env.PASSWORD,
     host : process.env.HOST,
-    database : process.env.DATABASE_URL,
+    database : process.env.DATABASE,
     ssl : process.env.ENV === 'PRODUCTION' ? {
             sslmode: 'require',
             rejectUnauthorized: false,
