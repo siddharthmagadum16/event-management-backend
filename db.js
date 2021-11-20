fbd4c6f321ea6981c10eafbde726ebc6042e2798
Siddharth Magadum <siddharthmagadum16@gmail.com>
2021-11-20 14:06:25 +0530

    changed database name to postgresql-perpendicular-42125

--- db.js
+++ db.js
@@ -6,7 +6,7 @@ const pool = new Pool ({
     user : process.env.USER,
     password : process.env.PASSWORD,
     host : process.env.HOST,
-    database : "event_management_system",
+    database : "postgresql-perpendicular-42125",
     ssl : process.env.ENV === 'PRODUCTION' ? {
             sslmode: 'require',
             rejectUnauthorized: false,
