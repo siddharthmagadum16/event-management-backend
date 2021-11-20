06c4e9a9cc539925d96590eb40fabfbfb35489d2
Siddharth Magadum <siddharthmagadum16@gmail.com>
2021-11-20 14:19:19 +0530

    database : from env.database_url

--- db.js
+++ db.js
@@ -6,7 +6,7 @@ const pool = new Pool ({
     user : process.env.USER,
     password : process.env.PASSWORD,
     host : process.env.HOST,
-    database : "postgresql-perpendicular-42125",
+    database : process.env.DATABASE_URL,
     ssl : process.env.ENV === 'PRODUCTION' ? {
             sslmode: 'require',
             rejectUnauthorized: false,
